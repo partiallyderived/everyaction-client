a0160dc906a8bed733628e69afa775749b612d67
Bobbey Reese <partiallyderived@gmail.com>
2022-04-14 20:20:59 -0400

    Specify version in generated docs

--- docs-src/conf.py
+++ docs-src/conf.py
@@ -13,7 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import re
 
 # -- Project information -----------------------------------------------------
 
@@ -22,8 +22,8 @@ copyright = '2021, Bobbey Reese'
 author = 'Bobbey Reese'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
-
+with open('../setup.cfg') as f:
+    release = re.search('version = (.*)', f.read()).group(1)
 
 # -- General configuration ---------------------------------------------------
 
--- inc_version.sh
+++ inc_version.sh
@@ -5,10 +5,6 @@ VERSION_STR=$(sed -nE 's/^version = (.*)$/\1/p' setup.cfg)
 IFS='.' read MAJOR MINOR PATCH <<< $VERSION_STR
 ((PATCH += 1))
 
-if [[ $PATCH == "7\r" ]]; then
-  echo yes
-fi
-
 NEW_VERSION=$MAJOR.$MINOR.$PATCH
 
 sed -i.bak -E "s/^version = .*/version = $NEW_VERSION/" setup.cfg
